e3380e28341bb0b4fe6b3e3d3a0f6ae75d7c9ad9
aidenfmunro <aidenfmunro@gmail.com>
2023-11-17 14:21:36 +0300

    spu header options

--- headers/spu.h
+++ headers/spu.h
@@ -6,7 +6,7 @@
 
 const int AMOUNT_OF_REGISTERS = 4;
 
-const int AMOUNT_OF_RAM_SLOTS = 4096;
+const int EXTRA_SPACE = 256;
 
 const int EXIT_CODE = -1;
 
@@ -19,6 +19,8 @@ const size_t RECT_HEIGHT = 20;
 const size_t RAM_HEIGHT = 30;
 
 const size_t RAM_WIDTH = 101;
+
+const int AMOUNT_OF_RAM_SLOTS = RAM_WIDTH * RAM_HEIGHT + EXTRA_SPACE;
 
 const double DELAY_FACTOR = 0.8;
 
